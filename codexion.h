8c6225bef09843b8cf5e44d8c602d2aed37ee60c
Rem <ankamas96@hotmail.fr>
2026-07-23 14:35:52 +0200

    Header + main started

---
+++ codexion.h
@@ -0,0 +1,12 @@
+#ifndef CODEXION_H
+#define CODEXION_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+
+#endif
