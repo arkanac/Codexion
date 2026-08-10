a0ff17e9b6e8cb56dbcde7d4b82ed668fb7748b2
Rem <ankamas96@hotmail.fr>
2026-08-10 18:49:00 +0200

    fixed incorrect logic in remove_from_queue

--- srcs/scheduler.c
+++ srcs/scheduler.c
@@ -6,7 +6,7 @@
 /*   By: rem <rem@student.42lyon.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/04 15:42:42 by repichan          #+#    #+#             */
-/*   Updated: 2026/08/10 18:37:27 by rem              ###   ########lyon.fr   */
+/*   Updated: 2026/08/10 18:47:42 by rem              ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,13 +32,13 @@ int	add_to_queue(t_coder *coder, t_dongle *dongle)
 
 int	remove_from_queue(t_coder *coder, t_dongle *dongle)
 {
-	if (dongle->queue[1] != 0 && dongle->queue[1] != coder->id )
+	if (dongle->queue[0] == coder->id)
 	{
-		ft_swap(&dongle->queue[1], &dongle->queue[0]);
+		dongle->queue[0] = dongle->queue[1];
 		dongle->queue[1] = 0;
 	}
-	else
-		coder->params->dongles[dongle->id].queue[0] = 0;
+	else if (dongle->queue[1] == coder->id)
+		dongle->queue[1] = 0;
 	return (0);
 }
 
